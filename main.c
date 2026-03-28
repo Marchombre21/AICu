f560c833e806e419c0449221bfe43754d422edab
Bruno <bruno.fitte@sfr.fr>
2026-03-28 14:32:51 +0100

    Comment useless code

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: bfitte <bfitte@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/28 07:06:19 by bfitte            #+#    #+#             */
-/*   Updated: 2026/03/28 13:51:30 by bfitte           ###   ########lyon.fr   */
+/*   Updated: 2026/03/28 14:32:29 by bfitte           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 int	main(void)
 {
 	t_vector	vec;
-	size_t	i;
+	// size_t	i;
 
 	vector_init(&vec, 50);
 	if (read_pro_max(&vec) == -1)
@@ -25,13 +25,12 @@ int	main(void)
 		printf("%i", vec.data[vec.num_elements - 1]);
 		return (1);
 	}
-	i = 0;
-	printf("%zu", vec.num_elements);
-	while (i < vec.num_elements)
-	{
-		printf("%u\n", vec.data[i]);
-		i++;
-	}
+	// i = 0;
+	// while (i < vec.num_elements)
+	// {
+	// 	printf("%u\n", vec.data[i]);
+	// 	i++;
+	// }
 	free(vec.data);
 	
 	return (0);
